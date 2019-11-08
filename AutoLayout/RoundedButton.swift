6e54fda9cdee469e3ffe54b5b10526c7682282cd
ChristianHurtado29 <christianhurtado@pursuit.org>
2019-11-08 10:56:59 -0500

    Ques 1

---
+++ AutoLayout/RoundedButton.swift
@@ -0,0 +1,28 @@
+//
+//  RoundedButton.swift
+//  AutoLayout
+//
+//  Created by Christian Hurtado on 11/7/19.
+//  Copyright © 2019 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 15
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = cornerRadius
+    }
+}
